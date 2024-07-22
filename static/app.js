20194840dc34dffe532d84c764e716c43bf08999
Biplab Datta <dattabiplab021@gmail.com>
2024-07-22 08:15:00 +0530

    changes in port app.js

--- static/app.js
+++ static/app.js
@@ -18,6 +18,13 @@ function getBHKValue() {
   return -1
 }
 
+function getPort(callback) {
+  var configUrl = "https://bangalore-house-price-prediction-0ay8.onrender.com/get_config";
+  $.get(configUrl, function (data, status) {
+    callback(data.port);
+  });
+}
+
 function onClickedEstimatePrice() {
   console.log("Estimate price button clicked")
   var sqft = document.getElementById("uiSqft")
@@ -28,7 +35,8 @@ function onClickedEstimatePrice() {
 
    // var url = "http://127.0.0.1:5000/predict_home_price"   //use this if u r NOT using nginx
 //  var url = "/api/predict_home_price" //use this if u r using nginx
-  var url = "https://bangalore-house-price-prediction-0ay8.onrender.com/api/predict_home_price"
+  // var url = "https://bangalore-house-price-prediction-0ay8.onrender.com/api/predict_home_price"
+  var url = `https://bangalore-house-price-prediction-0ay8.onrender.com:${port}/api/predict_home_price`;
   $.post(
     url,
     {
@@ -50,7 +58,8 @@ function onPageLoad() {
   console.log("document loaded")
      // var url = "http://127.0.0.1:5000/get_location_names"	//use this if u r NOT using nginx
 //  var url = "/api/get_location_names" //use this if u r using nginx
-   var url = "https://bangalore-house-price-prediction-0ay8.onrender.com/api/get_location_names"
+   // var url = "https://bangalore-house-price-prediction-0ay8.onrender.com/api/get_location_names"
+  var url = `https://bangalore-house-price-prediction-0ay8.onrender.com:${port}/api/get_location_names`;
   $.get(url, function (data, status) {
     console.log("got response for get_location_names request")
     if (data) {
